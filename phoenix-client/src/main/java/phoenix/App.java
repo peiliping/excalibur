e975912e6cd7ff8ff32f309b81e26dc6a99c9304
peiliping <peilipingplp@gmail.com>
2015-05-19 15:32:17 +0800

    update

--- phoenix-client/src/main/java/phoenix/App.java
+++ phoenix-client/src/main/java/phoenix/App.java
@@ -6,11 +6,9 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import phoenix.config.Config;
 import phoenix.config.Context;
+import phoenix.service.DualService;
 import phoenix.util.Constants;
 import phoenix.util.InitTool;
-import ch.qos.logback.classic.LoggerContext;
-import ch.qos.logback.classic.joran.JoranConfigurator;
-import ch.qos.logback.core.joran.spi.JoranException;
 
 import com.google.common.base.Preconditions;
 
@@ -25,7 +23,9 @@ public class App {
 
         InitTool.initLogBack(Config.getContext().getString(Constants.CONF_LOGCONFIG_ITEM));
         Config.setApplicationContext(new ClassPathXmlApplicationContext(Constants.CONF_SPRING_ITEM));
-
+        
+        DualService ds =  Config.getApplicationContext().getBean(DualService.class);
+        System.out.println(ds.queryDual());
 
     }
 }
--- phoenix-client/src/main/java/phoenix/dao/DualDao.java
+++ phoenix-client/src/main/java/phoenix/dao/DualDao.java
@@ -3,12 +3,8 @@ package phoenix.dao;
 import java.util.List;
 import java.util.Map;
 
-import phoenix.dataObject.MultiResult;
-
 public interface DualDao {
 
     public List<String> query(Map<String, Object> paramMap);
-
-    public List<MultiResult> queryPhoenix(Map<String, Object> paramMap);
 
 }

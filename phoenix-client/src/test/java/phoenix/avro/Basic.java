694036877a9b9b995912e228cc0fd2d7c6775cf0
peiliping <peilipingplp@gmail.com>
2015-10-12 11:01:07 +0800

    update

--- phoenix-client/src/test/java/phoenix/avro/Basic.java
+++ phoenix-client/src/test/java/phoenix/avro/Basic.java
@@ -3,13 +3,16 @@ package phoenix.avro;
 import phoenix.avro.annotation.LogOrder;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
+@ToString(includeFieldNames = false)
 public class Basic {
-
     @Setter
     @Getter
-    @LogOrder(order = 1, skip = false)
-    private int idSvvvv;
-
-
+    @LogOrder(order = 1000, skip = false)
+    private String vn;
+    @Setter
+    @Getter
+    @LogOrder(order = 100, skip = false)
+    private int    idSvvvv;
 }
--- phoenix-client/src/test/java/phoenix/avro/Main.java
+++ phoenix-client/src/test/java/phoenix/avro/Main.java
@@ -1,13 +1,21 @@
 package phoenix.avro;
 
+import java.lang.reflect.Field;
+
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws NoSuchFieldException, SecurityException, IllegalArgumentException, IllegalAccessException {
 
-         SchemaBuilder op = new SchemaBuilder(TestAvro.class);
-         System.out.println(op.convert2Schema().toString(true));
-//        TestAvro ta = new TestAvro();
-        
+        // SchemaBuilder op = new SchemaBuilder(TestAvro.class);
+        // System.out.println(op.convert2Schema().toString(true));
+        TestAvro ta = new TestAvro();
+        Basic b = new Basic();
+        b.setIdSvvvv(23);
+        b.setVn("sdfsdf");
 
+        Class c = ta.getClass().getSuperclass();
+        Field f = c.getDeclaredField("vn");
+        f.setAccessible(true);
+        System.out.println(f.get(ta));
     }
 }

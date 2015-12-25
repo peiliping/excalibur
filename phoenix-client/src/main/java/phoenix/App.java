ea138aaa67955c3f598ba16daad446ba0b296995
peiliping <peilipingplp@gmail.com>
2015-12-25 11:36:41 +0800

    update

--- phoenix-client/src/main/java/phoenix/App.java
+++ phoenix-client/src/main/java/phoenix/App.java
@@ -6,9 +6,6 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import phoenix.config.Config;
 import phoenix.config.Context;
-import phoenix.service.DualService;
-import phoenix.service.ITestLogService;
-import phoenix.service.Test2LogService;
 import phoenix.util.Constants;
 import phoenix.util.InitTool;
 
@@ -26,22 +23,7 @@ public class App {
         InitTool.initLogBack(Config.getContext().getString(Constants.CONF_LOGCONFIG_ITEM));
         Config.setApplicationContext(new ClassPathXmlApplicationContext(Constants.CONF_SPRING_ITEM));
 
-        ITestLogService s = Config.getApplicationContext().getBean(ITestLogService.class);
-        Test2LogService s2 = Config.getApplicationContext().getBean(Test2LogService.class);
-        DualService s3 = Config.getApplicationContext().getBean(DualService.class);
-        while (true) {
-            try {
-                s.log();
-                s.log2();
-            } catch (Exception e) {
-            }
-            try {
-                s2.log();
-                s2.log2();
-            } catch (Exception e) {
-            }
-            Thread.sleep(100);
-            s3.queryDual();
-        }
+        LOG.info("=================START=================");
+
     }
 }

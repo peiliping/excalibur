ebfde0622ef66ff0c6991ac4574939348a86cdea
peiliping <peilipingplp@gmail.com>
2016-05-30 12:00:20 +0800

    update 4 intervel

--- icesword-agent/src/main/java/icesword/agent/Startup.java
+++ icesword-agent/src/main/java/icesword/agent/Startup.java
@@ -19,26 +19,26 @@ public class Startup {
 
     public static String       DEBUG_IP      = null;
 
+    public static int          INTERVEL      = 1000;
+
     public static void main(String[] args) throws Exception {
 
         Options options =
                 (new Options()).addOption("m", "mode", true, "offline or online").addOption("r", "remoteAddress", true, "config Server Ip .")
-                        .addOption("d", "debug", false, "debug").addOption("p", "debugip", true, "the ip 4 debug");
+                        .addOption("d", "debug", false, "debug").addOption("p", "debugip", true, "the ip 4 debug").addOption("i", "intervel", true, "jstat intervel");
         CommandLine commandLine = (new DefaultParser()).parse(options, args);
 
-        Boolean debug = commandLine.hasOption('d');
-        DEBUG = (debug != null && Boolean.valueOf(debug));
-
-        String debugIp = commandLine.getOptionValue("p");
-        DEBUG_IP = (debugIp != null ? debugIp : DEBUG_IP);
+        DEBUG = commandLine.hasOption('d');
+        DEBUG_IP = commandLine.getOptionValue("p", DEBUG_IP);
 
         JstatPlus jstat = null;
         MODE = Mode.getMode(commandLine.getOptionValue('m'));
+        INTERVEL = Integer.valueOf(commandLine.getOptionValue("i", "1000"));
 
         if (MODE == Mode.OFF_LINE) { // 单机模式
-            jstat = new JstatPlusOffline(commandLine, 1000 * 1);
+            jstat = new JstatPlusOffline(commandLine, INTERVEL);
         } else if (MODE == Mode.ON_LINE) { // Diamond模式
-            jstat = new JstatPlusOnline(commandLine, 1000 * 2);
+            jstat = new JstatPlusOnline(commandLine, INTERVEL * 2);
         }
 
         jstat.run();
